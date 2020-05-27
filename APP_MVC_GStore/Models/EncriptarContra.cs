63be4d6fc93380698266c4e1ebcec95ca22cf1e2
Dxos <i201816987@cibertec.edu.pe>
2020-05-27 16:08:18 -0500

    Logueo y Registro de usuarios

--- APP_MVC_GStore/Models/EncriptarContra.cs
+++ APP_MVC_GStore/Models/EncriptarContra.cs
@@ -6,11 +6,11 @@ using System.Web;
 
 namespace APP_MVC_GStore.Models
 {
-    public class EncriptarContra
+    public static class EncriptarContra
     {
-        public static string textToEncrypt(string paasWord)
+        public static string textToEncrypt(string passWord)
         {
-            return Convert.ToBase64String(System.Security.Cryptography.SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(paasWord)));
+            return Convert.ToBase64String(System.Security.Cryptography.SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(passWord)));
         }
     }
 }
