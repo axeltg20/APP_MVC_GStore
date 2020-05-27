a1b27b3f8a929dfc10669bb06256a5dd84967e78
Alexander Leon <i201816987@cibertec.edu.pe>
2020-05-27 16:23:36 -0500

    Revert "Revert "Logueo y Registro de usuarios""
    This reverts commit b4991916cd5e1ab179fa4a37fe768d898b3b11b3.

--- APP_MVC_GStore/Controllers/RegistroController.cs
+++ APP_MVC_GStore/Controllers/RegistroController.cs
@@ -9,6 +9,8 @@ namespace APP_MVC_GStore.Controllers
 {
     public class RegistroController : Controller
     {
+        /*Conectar BD*/
+        GameStoreEntities db = new GameStoreEntities();
         // GET: Registro
         public ActionResult Index()
         {
@@ -21,12 +23,47 @@ namespace APP_MVC_GStore.Controllers
         }
         /*Registrar Usuario*/
         [HttpPost]
-        public ActionResult Registrar(TB_Usuario user)
+        public ActionResult Registrar(string nombre, string apellido, string email, string contra)
         {
             /*Email not verified on registration time*/
-            user.emailverficiado = false;
+            //user.emailverficiado = false;
             /*It Generar unique code*/
+            //user.codigoactivacion = Guid.NewGuid();
+            /*Cifrado de contraseña*/
+            /*user.contra = APP_MVC_GStore.Models.EncriptarContra.textToEncrypt(user.contra);
+            db.TB_Usuario.Add(user);
+            db.SaveChanges();
+            return View();*/
+            ViewBag.nombre = nombre;
+            ViewBag.apellido = apellido;
+            ViewBag.email = email;
+            ViewBag.contra = contra;
+            if (!string.IsNullOrEmpty(nombre) && !string.IsNullOrEmpty(apellido) && !string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(contra))
+            {
+                var registrar = db.usp_CrearUsuario(nombre, apellido, email, contra);
+                return RedirectToAction("Ingresar", "Registro");
+
+            }
             return View();
         }
+      
+
+
+        
+        public ActionResult Ingresar(string email, string contra)
+               
+        {
+            ViewBag.email = email;
+            ViewBag.contra = contra;
+            if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(contra))
+            {
+                var logueo = db.usp_Logueo(email, contra);
+                return RedirectToAction("Index", "Producto");
+
+            }
+
+            return View();
+        }
+     }
+
     }
-}
